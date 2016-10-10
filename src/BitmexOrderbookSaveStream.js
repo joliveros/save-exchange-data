fc11a5dbfbaa1776469ef0182933c42c6cf3b56c
José Oliveros <jose.oliveros.1983@gmail.com>
2016-10-10 03:11:20 -0500

    parse string to json in BitmexOrderbookSaveStream

---
+++ src/BitmexOrderbookSaveStream.js
@@ -0,0 +1,17 @@
+import keys from 'lodash/keys';
+import prettyjson from 'prettyjson';
+import through2 from 'through2';
+import split2 from 'split2';
+
+export default function BitmexOrderbookSaveStream() {
+  const stream = through2({ objectMode: true }, function (chunk, enc, done) {
+    const data = JSON.parse(JSON.parse(chunk.toString()));
+
+    this.push(data);
+
+    done();
+  });
+
+  return split2()
+    .pipe(stream);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import { argv } from 'yargs'
+import { WsOrderBookStream } from 'bitmex-streams';
+import BitmexOrderbookSaveStream from './BitmexOrderbookSaveStream';
 
+const {
+  key: AWS_KEY,
+  secret: AWS_SECRET
+} = argv;
+
+const orderbookStream = new WsOrderBookStream();
+const bitmexOrderbookSaveStream = new BitmexOrderbookSaveStream();
+
+orderbookStream
+  .pipe(bitmexOrderbookSaveStream);
