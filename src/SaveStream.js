666f361e08f3ba920c0c08deb0770fd26e1fcd9b
José Oliveros <jose.oliveros.1983@gmail.com>
2016-10-11 00:44:04 -0500

    add SaveStream

---
+++ src/SaveStream.js
@@ -0,0 +1,43 @@
+import { argv } from 'yargs';
+import has from 'lodash/has';
+import split2 from 'split2';
+import through2 from 'through2';
+import InfluxDb from 'influxdb-nodejs';
+
+const {
+  DBHOST = 'localhost',
+} = argv;
+
+const dbClient = new InfluxDb(`http://${DBHOST}:8086/bitmex`);
+
+dbClient.createDatabase();
+
+async function save({ data, tagKey, tag, filter = [], domain }) {
+  // save only when data has [ 'table', 'action', 'data' ]
+  if (has(data, filter)) {
+    const Tag = data[tagKey] || tag;
+    return await dbClient.writePoint(domain, { data: JSON.stringify(data) }, Tag);
+  }
+  return null;
+}
+
+function BitmexOrderbookSaveStream({ domain, filter, tagKey, tag }) {
+  const stream = through2({ objectMode: true }, function (chunk, enc, done) {
+    const data = JSON.parse(JSON.parse(chunk.toString()));
+    const ctx = this;
+
+    save({ data, tagKey, tag, filter, domain })
+      .then(() => {
+        ctx.push(chunk);
+        done();
+      })
+      .catch((e) => {
+        done(e);
+      });
+  });
+
+  return split2()
+  .pipe(stream);
+}
+
+export default BitmexOrderbookSaveStream;
